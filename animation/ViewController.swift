7b5b5902bc01c0bc21ebbf84a86ddfced994e51f
navimalhotra <navimalhotra124@gmail.com>
2020-06-08 18:10:20 -0400

    Initial Commit

---
+++ animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by Navi Malhotra on 2020-06-08.
+//  Copyright © 2020 Naveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
